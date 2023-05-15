b2980b61c0afcf49f85ab1c044ed580c7fe60cff
Ihimbru-k <kanihims25@gmail.com>
2023-05-15 12:10:19 +0100

    modifications

--- lib/home.dart
+++ lib/home.dart
@@ -2,30 +2,60 @@ import 'package:flutter/material.dart';
 
 import 'colors/colors.dart';
 
+
 void main() {
-  runApp(const MyApp());
+  runApp(const Home());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: tdBgcolor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(Icons.menu, color: tdBlack, size: 30,),
+          Container(height: 40, width: 40, child: ClipRRect(borderRadius: BorderRadius.circular(20), child : Image.asset('assets/images/kboy.jpg')),)
+        ],
+      ),
+    );
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
         backgroundColor: tdBgcolor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        appBar: _buildAppBar(),
+        body: Container(child : Column(
           children: [
-            const Icon(Icons.menu, color: tdBlack, size: 30,),
-            Container(height: 40, width: 40, child: ClipRRect(borderRadius: BorderRadius.circular(20), child : Image.asset('assets/images/kboy.jpg')),)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(Icons.search, color: tdBlack, size : 20),
+                  prefixIconConstraints: BoxConstraints(
+                    maxHeight: 20,
+                    minWidth: 25,
+                  ),
+                  border: InputBorder.none,
+                  hintText : 'Search',
+                  hintStyle : TextStyle(color: tdGrey)
+
+                ),
+
+              ),
+            )
           ],
-        ),
-      ),
-
-      body: ,
-
+        ))
     );
   }
 }
